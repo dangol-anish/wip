04ec344543a8ca36f0453fd936bb521d246c3fb8
Anish Dangol <dangol.anish001@gmail.com>
2023-02-25 23:08:01 +0545

    .

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -4,11 +4,11 @@ import { RxAvatar } from "react-icons/rx";
 const Register = () => {
   return (
     <div className="h-screen flex items-center justify-center bg-gradient-to-r from-black to-[#2D3436] ">
-      <div className=" pt-12 pb-5 px-16 rounded-xl flex flex-col  bg-black/50">
+      <div className=" pt-12 pb-5 px-16 rounded-xl flex flex-col  bg-black/30">
         <h1 className="text-white text-3xl font-bold">Let's Get Started</h1>
         <p className="text-white text-md pb-10">Create Your Account</p>
-        <div className="flex flex-col items-center gap-4">
-          <form className="flex flex-col gap-5  items-center ">
+        <div className="flex flex-col items-center gap-6">
+          <form className="flex flex-col gap-4  items-center ">
             <input
               className="bg-zinc-800 p-4 rounded-xl text-white focus:outline-none "
               type="text"
@@ -38,7 +38,7 @@ const Register = () => {
               <RxAvatar size={20} className="text-white " />
               Add an Avatar
             </label>
-            <button class="ease-in-out duration-1000 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
+            <button class="mt-5 ease-in-out duration-1000 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
               <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                 Sign Up
               </span>
